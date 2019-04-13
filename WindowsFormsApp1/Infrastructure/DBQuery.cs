eefa72c332d2b213ff9a2ab40e65c446cc112210
Zack <zackbotkin@gmail.com>
2019-04-12 22:40:58 -0500

    clean up sql connection a bit

--- WindowsFormsApp1/Infrastructure/DBQuery.cs
+++ WindowsFormsApp1/Infrastructure/DBQuery.cs
@@ -11,14 +11,26 @@ namespace WindowsFormsApp1.Infrastructure
     public class DBQuery
     {
 
-        private string connString = @"Data Source=DESKTOP-90E0LOH\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True";
+        private readonly string _dataSource = @"DESKTOP-90E0LOH\SQLEXPRESS";
+        private readonly string _initialCatalog = "Test";
+        private readonly bool _integratedSecurity = true;
+        private string _connString;
+        
+        public DBQuery()
+        {
+            _connString = $"Data Source={_dataSource};Initial Catalog={_initialCatalog};Integrated Security={_integratedSecurity}";
+        }
 
+        public DBQuery(string connString)
+        {
+            _connString = connString;
+        } 
 
         public DataTable GetData()
         {
             DataTable table = new DataTable();
 
-            using (SqlConnection sqlConn = new SqlConnection(connString))
+            using (SqlConnection sqlConn = new SqlConnection(_connString))
             {
                 string sqlQuery = @"SELECT * FROM Account";
                 using (SqlCommand cmd = new SqlCommand(sqlQuery, sqlConn))
@@ -30,6 +42,8 @@ namespace WindowsFormsApp1.Infrastructure
 
             return table;
         }
+
+
 
     }
 }
